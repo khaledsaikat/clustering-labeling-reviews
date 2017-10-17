e6df56c5d7e4fcf590b638f8bc719280f8f9877e
Khaled <khaledsaikat@gmail.com>
2017-10-17 18:54:51 +0200

    Implemented multigram weight

--- aspects/centroid_labels.py
+++ aspects/centroid_labels.py
@@ -2,6 +2,7 @@
 import operator
 from typing import List, Tuple, Union, Dict
 
+import data_loader as dl
 import numpy as np
 import word2vec as w2v
 from nltk.corpus import stopwords
@@ -22,6 +23,7 @@ cl.inspect_labels()
 Word = str
 Term = Union[Word, Tuple[Word, Word], Tuple[Word, Word, Word]]
 Document = TextBlob
+TokenizeDocument = List[Term]
 DocumentsCluster = List[Document]
 TokensCluster = List[Term]
 TokenizeDocCluster = List[List[Term]]
@@ -35,7 +37,7 @@ class NormalizeClusters:
         self.normalize(clusters)
 
     @property
-    def clusters(self) -> List[List[Document]]:
+    def clusters(self) -> List[DocumentsCluster]:
         return self.__clusters
 
     def normalize(self, clusters: List[List[str]]):
@@ -57,25 +59,25 @@ class NormalizeClusters:
 
 
 class TokenizeSingleCluster:
-    __cluster: List[List[Term]] = []
+    __cluster: TokenizeDocCluster = []
 
-    def __init__(self, cluster: List[Document], n_gram: int = 1):
+    def __init__(self, cluster: DocumentsCluster, n_gram: int = 1):
         self.n_gram: int = n_gram
         self.__tokenize(cluster)
 
-    def __tokenize(self, cluster: List[Document]) -> List[List[Term]]:
+    def __tokenize(self, cluster: DocumentsCluster):
         if self.n_gram > 1:
             self.__cluster = [[tuple(x) for x in doc.ngrams(n=self.n_gram)] for doc in cluster]
         else:
             self.__cluster = [doc.words for doc in cluster]
 
     @property
-    def cluster(self) -> List[List[Term]]:
+    def cluster(self) -> TokenizeDocCluster:
         """Return tokenized documents"""
         return self.__cluster
 
     @property
-    def tokens(self) -> List[Term]:
+    def tokens(self) -> TokensCluster:
         """Combined all tokens"""
         return [term for doc in self.__cluster for term in doc]
 
@@ -83,12 +85,12 @@ class TokenizeSingleCluster:
 class TokenizeClusters:
     """Tokenize all clusters"""
 
-    def __init__(self, clusters: List[List[Document]], n_gram: int = 1):
+    def __init__(self, clusters: List[DocumentsCluster], n_gram: int = 1):
         self.__clusters: List[TokenizeSingleCluster] = []
         self.n_gram: int = n_gram
         self.__tokenize(clusters)
 
-    def __tokenize(self, clusters: List[List[Document]]):
+    def __tokenize(self, clusters: List[DocumentsCluster]):
         self.__clusters = [TokenizeSingleCluster(cluster, self.n_gram) for cluster in clusters]
 
     @property
@@ -97,17 +99,17 @@ class TokenizeClusters:
         return self.__clusters
 
     @property
-    def documents_clusters(self) -> List[List[List[Term]]]:
+    def documents_clusters(self) -> List[TokenizeDocCluster]:
         """Get clusters of tokenized documents"""
         return [[doc for doc in cluster_object.cluster] for cluster_object in self.__clusters]
 
     @property
-    def tokens_clusters(self) -> List[List[Term]]:
+    def tokens_clusters(self) -> List[TokensCluster]:
         """Get clusters of tokens"""
         return [[token for token in cluster_object.tokens] for cluster_object in self.__clusters]
 
     @property
-    def documents(self) -> List[List[Term]]:
+    def documents(self) -> List[TokenizeDocument]:
         """Get list of all tokenized documents"""
         return [doc for cluster_object in self.__clusters for doc in cluster_object.cluster]
 
@@ -123,12 +125,38 @@ def idf_tokens_weights(clusters: TokenizeClusters) -> Dict[Term, float]:
     return {token: TFIDF(token).idf for token in clusters.tokens}
 
 
+class ClustersTokensWeight:
+    """Get clusters of tokens weight"""
+
+    def __init__(self, clusters: List[DocumentsCluster], n_gram: int):
+        self.tokenize_clusters = TokenizeClusters(clusters, n_gram)
+        self.idf_weights = idf_tokens_weights(self.tokenize_clusters)
+        self.clusters_tokens_weights = self.calculate_tokens_weights()
+
+    def calculate_tokens_weights(self) -> List[Dict[Term, float]]:
+        return [{token: self.__calculate_tfidf(token, cluster) for token in cluster} for cluster
+                in self.tokenize_clusters.tokens_clusters]
+
+    def __calculate_tfidf(self, token: Term, cluster: TokensCluster) -> float:
+        tf = TFIDF(token, cluster).tf
+        return tf * self.idf_weights[token]
+
+    @property
+    def clusters(self) -> List[Dict[Term, float]]:
+        return self.clusters_tokens_weights
+
+
 class UnigramCentroidLabel:
     idf_weights: Dict[Term, float] = {}
 
+    clusters_documents: List[DocumentsCluster] = []
+
     clusters_tokens_weights: List[Dict[Term, float]] = []
 
+    clusters_docs_weight: List[List[float]] = []
+
     def __init__(self, clusters: List[DocumentsCluster]):
+        self.clusters_documents = clusters
         self.tokenized_clusters = TokenizeClusters(clusters)
         self.idf_weights = idf_tokens_weights(self.tokenized_clusters)
         self.calculate_tokens_weights()
@@ -141,33 +169,102 @@ class UnigramCentroidLabel:
         tf = TFIDF(token, cluster).tf
         return tf * self.idf_weights[token]
 
-    def calculate_sentence_weight(self, cluster: TokenizeDocCluster):
-        pass
+    def calculate(self):
+        self.clusters_docs_weight = [self.calculate_sentence_weight(cluster, index) for index, cluster in
+                                     enumerate(self.tokenized_clusters.documents_clusters)]
+
+    def calculate_sentence_weight(self, cluster: TokenizeDocCluster, cluster_index: int) -> List[float]:
+        return [sum(self.clusters_tokens_weights[cluster_index][token] for token in doc) for doc in cluster]
+
+    @staticmethod
+    def sentence_weight(tokenize_sentence: List[int]):
+        return tokenize_sentence
+
+        # @staticmethod
+        # def __max_list_value(my_list: List[float]) -> Tuple(int, float):
+        #    """Return max value of a list and its index (index:value)"""
+        #    return max(enumerate(my_list), key=operator.itemgetter(1))
 
 
-class LabelSentance:
-    def __init__(self, cluster):
-        pass
+class UnigramSentenceWeightModel:
+    """Clusters of unigram weighted sentences"""
+
+    def __init__(self, clusters: List[DocumentsCluster]):
+        self.clusters_documents: List[DocumentsCluster] = clusters
+        self.clusters_tokens_weight: List[Dict[Term, float]] = ClustersTokensWeight(clusters, 1).clusters
+
+    def single_sentence_weight(self, sentence: Document, cluster_index: int) -> float:
+        return self.__sum_tokens_weight(sentence.words, cluster_index)
+
+    def __sum_tokens_weight(self, tokens: List[Term], cluster_index: int) -> float:
+        if len(tokens) < 1:
+            return 0
+        return sum(self.clusters_tokens_weight[cluster_index][token] for token in tokens) / len(tokens)
+
+    @property
+    def clusters(self) -> List[List[float]]:
+        return [[self.single_sentence_weight(doc, cluster_index) for doc in cluster] for cluster_index, cluster in
+                enumerate(self.clusters_documents)]
 
 
-def get_label_sentance(self):
-    sent_weight_clusters = [[self.__sent_weight(sent, cluster_index) for sent in cluster] for clusterIndex, cluster
-                            in
-                            enumerate(self.clusters)]
-    for cluster_index, sent_weight_list in enumerate(sent_weight_clusters):
-        sent_index, value = self.max_list_value_index(sent_weight_list)
-        print(cluster_index, self.rawClusters[cluster_index][sent_index])
+class MultigramSentenceWeightModel:
+    """Clusters of multigram weighted sentences"""
+
+    def __init__(self, clusters: List[DocumentsCluster]):
+        self.clusters_documents: List[DocumentsCluster] = clusters
+        self.clusters_tokens_weight: List[Dict[Term, float]] = self.combine_tokens_weight(clusters)
+
+    @staticmethod
+    def combine_tokens_weight(clusters: List[DocumentsCluster]):
+        unigram_clusters = ClustersTokensWeight(clusters, 1).clusters
+        bigram_clusters = ClustersTokensWeight(clusters, 2).clusters
+        trigram_clusters = ClustersTokensWeight(clusters, 3).clusters
+        for index, cluster in enumerate(unigram_clusters):
+            unigram_clusters[index].update(bigram_clusters[index])
+            unigram_clusters[index].update(trigram_clusters[index])
+
+        return unigram_clusters
+
+    def single_sentence_weight(self, sentence: Document, cluster_index: int) -> float:
+        uigram = sentence.words
+        bigram = [tuple(x) for x in sentence.ngrams(n=2)]
+        trigram = [tuple(x) for x in sentence.ngrams(n=3)]
+
+        return self.__sum_tokens_weight(uigram, cluster_index) \
+               + self.__sum_tokens_weight(bigram, cluster_index) \
+               + self.__sum_tokens_weight(trigram, cluster_index)
+
+    def __sum_tokens_weight(self, tokens: List[Term], cluster_index: int) -> float:
+        if len(tokens) < 1:
+            return 0
+        return sum(self.clusters_tokens_weight[cluster_index][token] for token in tokens) / len(tokens)
+
+    @property
+    def clusters(self) -> List[List[float]]:
+        return [[self.single_sentence_weight(doc, cluster_index) for doc in cluster] for cluster_index, cluster in
+                enumerate(self.clusters_documents)]
 
 
-def max_list_value_index(self, my_list):
-    """Return max value of a list and its index"""
-    return max(enumerate(my_list), key=operator.itemgetter(1))
+class ClustersLabels:
+    def __init__(self, raw_clusters: List[List[str]]):
+        self.raw_clusters: List[List[str]] = raw_clusters
+        self.clusters_documents: List[DocumentsCluster] = NormalizeClusters(raw_clusters).clusters
+        self.clusters_weights = UnigramSentenceWeightModel(self.clusters_documents).clusters
+        #self.clusters_weights = MultigramSentenceWeightModel(self.clusters_documents).clusters
 
+    @staticmethod
+    def __max_value(my_list) -> Tuple[int, float]:
+        """Return max value of a list and its index"""
+        return max(enumerate(my_list), key=operator.itemgetter(1))
 
-def __sent_weight(self, sent, cluster_index):
-    if len(sent.words) is 0:
-        return 0
-    return sum(self.tokensWeight[cluster_index][word] for word in sent.words) / len(sent.words)
+    @property
+    def clusters_index(self) -> List[Tuple[int, float]]:
+        return [self.__max_value(cluster) for cluster in self.clusters_weights]
+
+    @property
+    def clusters(self) -> List[Document]:
+        return [self.raw_clusters[cluster_index][self.__max_value(cluster)[0]] for cluster_index, cluster in
+                enumerate(self.clusters_weights)]
 
 
 class W2VCentroidLabel:
@@ -214,29 +311,30 @@ class ClustersCentroidLabels:
         UnigramCentroidLabel(self.clusters)
 
 
+def compare_labels(pre_labels: List[str], post_labels: List[str]):
+    for index, label in enumerate(pre_labels):
+        print(label, ":", post_labels[index])
+
+
 def inspect_labels():
     data = {
         "a": ["I'm a  sound bit, of a headphone/earphone snob.",
               "these are super cheap. and mostly you get what you pay for."],
         "b": ["Unbelievable sound for the price", "they sound great and are built well."]
     }
-    # data = dl.loadJsonFromFile("../data/headphone_clusters.json")
+    data = dl.loadJsonFromFile("../data/headphone_clusters.json")
 
     pre_labels = [k for k, v in data.items()]
     raw_clusters = [v for k, v in data.items()]
 
-    clusters = NormalizeClusters(raw_clusters).clusters
+    # clusters = NormalizeClusters(raw_clusters).clusters
     # clusters = TokenizeClusters(clusters).documents_clusters
-    # print(clusters)
-    # return
 
-    UnigramCentroidLabel(clusters)
-    return
+    # UnigramCentroidLabel(clusters)
+    # c = MultigramSentenceWeightModel(clusters).clusters
 
-    for index, cluster in enumerate(clusters):
-        cl = CentroidLabel(cluster)
-        label = raw_clusters[index][cl.get_closest_doc_index()]
-        print(pre_labels[index], ":", label)
+    labels = ClustersLabels(raw_clusters).clusters
+    compare_labels(pre_labels, labels)
 
 
 if __name__ == "__main__":
