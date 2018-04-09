a36e597df4651feb61baad7e2502b4686edf143b
Khaled <khaledsaikat@gmail.com>
2018-04-09 15:51:06 +0200

    Remove aspects and tests dir

--- aspects/main.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from centroid_labels import inspect_labels
-
-if __name__ == "__main__":
-    inspect_labels()
--- tests/context.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from aspects import helpers
-from aspects.cluster_label import ClusterLabel
